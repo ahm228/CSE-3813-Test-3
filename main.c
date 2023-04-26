db907ba474ce41c2703adc6cf99ba700c3cd8bb1
ahm228 <94765616+ahm228@users.noreply.github.com>
2023-04-26 13:04:32 -0500

    Consolidation
    Consolidated binary_sem.c into main.c

--- main.c
+++ main.c
@@ -8,6 +8,34 @@
 #include "semun.h"
 #include "binary_sem.h"
 
+// Binary semaphore functions
+int set_semvalue(int sem_id, int sem_num, int val) {
+    union semun sem_union;
+    sem_union.val = val;
+    return semctl(sem_id, sem_num, SETVAL, sem_union);
+}
+
+void del_semvalue(int sem_id) {
+    union semun sem_union;
+    semctl(sem_id, 0, IPC_RMID, sem_union);
+}
+
+int reserve_semaphore(int sem_id, int sem_num) {
+    struct sembuf sem_b;
+    sem_b.sem_num = sem_num;
+    sem_b.sem_op = -1;
+    sem_b.sem_flg = SEM_UNDO;
+    return semop(sem_id, &sem_b, 1);
+}
+
+int release_semaphore(int sem_id, int sem_num) {
+    struct sembuf sem_b;
+    sem_b.sem_num = sem_num;
+    sem_b.sem_op = 1;
+    sem_b.sem_flg = SEM_UNDO;
+    return semop(sem_id, &sem_b, 1);
+}
+
 typedef struct {
     int num_blocks;
     struct {
